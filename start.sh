36e954c04633a6c5c098c1cdf7a791367fdf8dbd
malixds <malixds@mail.ru>
2024-08-31 20:45:05 +0300

    fixes

--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Остановить скрипт при любой ошибке
 set -e
 
-# Запуск контейнеров в фоновом режиме с пересборкой
 docker-compose up -d --build
 
-# Подождите несколько секунд, чтобы PostgreSQL был готов
 echo "Waiting for PostgreSQL"
 sleep 10
 
-# Создание таблицы в базе данных PostgreSQL
 echo "Creating users table"
 
 docker exec -i postgres psql -U test -d test <<EOF
@@ -19,5 +15,3 @@ CREATE TABLE IF NOT EXISTS users (
     balance BIGINT
 );
 EOF
-# Вывод списка контейнеров для проверки
-docker ps
